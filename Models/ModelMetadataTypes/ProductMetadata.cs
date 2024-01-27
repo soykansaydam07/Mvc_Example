ab7a6e43e0d3934da7bfdd3ea0d402216b61aa66
soykansaydam07 <soykansaydam1@gmail.com>
2024-01-28 00:20:24 +0300

    ..

---
+++ Models/ModelMetadataTypes/ProductMetadata.cs
@@ -0,0 +1,10 @@
+﻿namespace Mvc_Example.Models.ModelMetadataTypes
+{
+    public class ProductMetadata
+    {
+        //Productdaki Data annotations verilerinin bulunduğu yeni bir kısım oluşturduk 
+        //[Required(ErrorMessage ="Lütfen product Name 'i giriniz")]
+        //[StringLength(100)]
+        public string ProductName { get; set; }
+    }
+}
